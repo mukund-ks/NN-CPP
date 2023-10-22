5247a8174ac702e5949eb6f0263c751bc6e7846e
Mukund Kumar <sankhyan.mukund@gmail.com>
2023-10-22 16:28:37 +0530

    Add destructor for base Layer class

--- include/layers.h
+++ include/layers.h
@@ -12,27 +12,28 @@ class Layer {
     Matrix biases;
 
    public:
+    virtual ~Layer() = default;
     virtual Matrix forward(const Matrix &input) const = 0;
     virtual Matrix backward(const Matrix &input) const = 0;
 };
 
 class InputLayer : public Layer {
    public:
-    InputLayer(int inputSize, int outputSize, ActivationFunction* activationFn);
+    InputLayer(int inputSize, int outputSize, ActivationFunction *activationFn);
     Matrix forward(const Matrix &input) const override;
     Matrix backward(const Matrix &input) const override;
 };
 
 class DenseLayer : public Layer {
    public:
-    DenseLayer(int inputSize, int outputSize, ActivationFunction* activationFn);
+    DenseLayer(int inputSize, int outputSize, ActivationFunction *activationFn);
     Matrix forward(const Matrix &input) const override;
     Matrix backward(const Matrix &input) const override;
 };
 
 class OutputLayer : public Layer {
    public:
-    OutputLayer(int inputSize, int outputSize, ActivationFunction* activationFn);
+    OutputLayer(int inputSize, int outputSize, ActivationFunction *activationFn);
     Matrix forward(const Matrix &input) const override;
     Matrix backward(const Matrix &input) const override;
 };
