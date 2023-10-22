bca4ae3d44c25129db865dccdb614e08aff8b995
Mukund Kumar <sankhyan.mukund@gmail.com>
2023-10-22 16:10:28 +0530

    Update header file for layers

--- include/layers.h
+++ include/layers.h
@@ -6,34 +6,33 @@
 typedef Eigen::MatrixXf Matrix;
 
 class Layer {
-   private:
-    ActivationFunction *activationFunction;
+   protected:
+    ActivationFunction *activationFn;
     Matrix weights;
     Matrix biases;
 
    public:
-    Layer(int inputSize, int outputSize);
-    virtual Matrix forward(const Matrix &input) const;
-    virtual Matrix backward(const Matrix &input) const;
+    virtual Matrix forward(const Matrix &input) const = 0;
+    virtual Matrix backward(const Matrix &input) const = 0;
 };
 
 class InputLayer : public Layer {
    public:
-    InputLayer(int inputSize, int outputSize);
+    InputLayer(int inputSize, int outputSize, ActivationFunction* activationFn);
     Matrix forward(const Matrix &input) const override;
     Matrix backward(const Matrix &input) const override;
 };
 
 class DenseLayer : public Layer {
    public:
-    DenseLayer(int inputSize, int outputSize);
+    DenseLayer(int inputSize, int outputSize, ActivationFunction* activationFn);
     Matrix forward(const Matrix &input) const override;
     Matrix backward(const Matrix &input) const override;
 };
 
 class OutputLayer : public Layer {
    public:
-    OutputLayer(int inputSize, int outputSize);
+    OutputLayer(int inputSize, int outputSize, ActivationFunction* activationFn);
     Matrix forward(const Matrix &input) const override;
     Matrix backward(const Matrix &input) const override;
 };
